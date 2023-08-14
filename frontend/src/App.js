e5c5f3e9e93b4fdbd6e21a58a37b3202c82476b7
AFNAN-091 <saiedafnan636@gmail.com>
2023-08-14 17:29:43 +0600

    add list of product info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <p>Welcome To AFnaN's Shop</p>
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <HomeScreen> </HomeScreen>
+        </Container>
+       </main>
+       <Footer/>
+    </>
   )
 }
 
